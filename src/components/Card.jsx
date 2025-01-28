3374bfa1eb3e35e1c5ae9cb76c9c417fff63f74d
l-martinetti <luca.martinetti@outlook.it>
2025-01-28 19:18:08 +0100

    feat: add card on search
    -Milestone 1 complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,23 @@
+import { useGlobalContext } from "../contexts/GlobalContext"
 
 const Card = () => {
+
+    const { films } = useGlobalContext();
+
     return (
-        <div className="card">
-            <img src="" alt="" />
-            <div className="">
-                <h4>Title</h4>
-                <p>Original Title</p>
-                <p>Language</p>
-                <p>Vote</p>
-            </div>
-        </div>
+        <>
+            {films.map(film => (
+                <div key={film.id} className="card">
+                    <div>
+                        <h4>{film.title}</h4>
+                        <p>{film.original_title}</p>
+                        <p>{film.original_language}</p>
+                        <p>{Math.round(film.vote_average)}</p>
+                    </div>
+                </div>
+            ))}
+
+        </>
     )
 }
 
